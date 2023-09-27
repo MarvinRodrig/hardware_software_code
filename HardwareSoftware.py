a2ade114e7a63bdf74de6236f5ea3547abfdfe76
MarvinRodrig <144740230+MarvinRodrig@users.noreply.github.com>
2023-09-27 12:08:03 -0400

    20230927 my first commit

---
+++ HardwareSoftware.py
@@ -0,0 +1,13 @@
+def my_function():
+println("Hello")
+print("what is your name?")
+name = input("Your name:")
+print("")
+
+name = "Professer Nedd"
+print( "We want to know if you like programming!" )
+print()
+print( "Do you like programming " + name + "?" )
+answer = input()
+print( "Great! You said" + answer + "?" )
+print( "Let's learn some Python today" )
