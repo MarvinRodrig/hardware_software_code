6e150eecc3d56d231acf91637936a3194f44b4e0
MarvinRodrig <144740230+MarvinRodrig@users.noreply.github.com>
2023-10-04 12:10:36 -0400

    The user adding their own numbers.

--- user_numbers.py
+++ user_numbers.py
@@ -4,12 +4,8 @@ def main():
     print("This program add two numbers.")
     number1 = input("Enter the first number: ")
     number2 = input("Enter the second number: ")
-    total = number1 + number2
-    print("The total is " + total + ".")
-
-
-
-
+    total = int(number1) + int(number2)
+    print("The total is " + str(total) + ".")
 
 
 if __name__ == "__main__":
