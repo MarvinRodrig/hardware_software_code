957c9bd0187a8acd1c49b2b46a6f9c6294a22eba
MarvinRodrig <144740230+MarvinRodrig@users.noreply.github.com>
2023-10-04 11:21:28 -0400

    Learning to add numbers in Python.

--- hardw0resoftware.py
+++ hardw0resoftware.py
@@ -12,6 +12,15 @@ def main():
     number2 = 20
     number2 = number1 + number2 + number3
     print(number2)
+
+    number1 = 125
+    number2 = 100
+    number3 = number1 - number2
+    print(number3)
+    number3 = 10
+    number1 = 20
+    number2 = number1 + number2 + number3
+    print(number3 + number2)
     
 if __name__ == "__main__":
     main()
