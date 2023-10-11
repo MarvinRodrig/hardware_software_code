f304a6f894fdafb34a8074574db65825dd5d11d8
MarvinRodrig <144740230+MarvinRodrig@users.noreply.github.com>
2023-10-11 11:35:15 -0400

    Pick a door to win a prize!!

--- choose_a_door.py
+++ choose_a_door.py
@@ -2,11 +2,11 @@ def choose_a_door():
     prize = 4
     message = "Choose the correct door 1, 2, 3: "
     print("Do you want to win a prize?")
-    door = int(input())
+    door = int(input(message))
 
     while door < 1 or door > 3:
         print("Invalid Door!")
-        door = int(input())
+        door = int(input(message))
     if door == 1:
         prize = 2 + 9 // 10 * 100
     elif door == 2:
