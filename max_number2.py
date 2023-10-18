1ad400be480945aa26bc2434b1615550e59901fe
MarvinRodrig <144740230+MarvinRodrig@users.noreply.github.com>
2023-10-18 11:19:13 -0400

    more maximum number code

---
+++ max_number2.py
@@ -0,0 +1,13 @@
+def max (num1, num2) :
+    if num1 >= num2 :
+        print("1:",num1)
+        return num1
+    print("2:",num2)
+    return num2
+
+def main():
+    larger = max(1,5)
+    
+if __name__ == "__main__":
+    main()
+
