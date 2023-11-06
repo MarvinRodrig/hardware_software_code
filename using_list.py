9b9272dc3d3989115e5c00bd039f2a63e45b98bb
MarvinRodrig <144740230+MarvinRodrig@users.noreply.github.com>
2023-11-06 11:22:39 -0500

    using list and average

---
+++ using_list.py
@@ -0,0 +1,15 @@
+def main():
+    nums = [3, 41, 12, 9, 74, 15]
+    print(len(nums))
+    print(max(nums))
+    print(min(nums))
+    print(sum(nums))
+    lens = len(nums)
+    sume = sum(nums)
+    avg = sume / lens
+    print(avg)
+
+# double slash is integer division and single slash is division with the ground 
+
+if __name__ == "__main__":
+    main()
